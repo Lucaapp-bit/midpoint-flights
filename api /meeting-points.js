f40b86bdb119cd7623a052a9108137226d02840e
Lucaapp-bit <luca.ventura33@gmail.com>
2026-08-03 15:30:45 +0300

    Delete api /meeting-points.js

--- api /meeting-points.js
+++
@@ -1,7 +0,0 @@
-import { searchAirports } from "./_airports.js";
-
-export default function handler(req, res) {
-  const q = req.query.q || "";
-  res.setHeader("Cache-Control", "public, max-age=3600");
-  res.status(200).json(searchAirports(String(q)));
-}
